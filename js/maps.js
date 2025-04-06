5edba15fe7000a2779f64d78c1be7292fdc082fd
Cynthia Gong <cynthiagong@g.ucla.edu>
2025-04-06 00:22:45 -0400

    Version 1 Updates

--- js/maps.js
+++ js/maps.js
@@ -8,9 +8,11 @@ let globaldata;
 
 // path to csv data
 
-let path = "data/shops.csv";
+let path = "data/locations.csv";
 
 let shopmarkers = L.featureGroup();
+let parkmarkers = L.featureGroup();
+let museummarkers = L.featureGroup();
 
 let shops;
 
@@ -40,35 +42,51 @@ function readCSV(path){
 		download: true,
 		complete: function(data) {
 			globaldata = data;
-			shops = globaldata.data;
-			console.log(shops.Latitude)
+			shops = globaldata.data.filter(data => data.Type=='shop');
+			parks = globaldata.data.filter(data => data.Type=='park');
+			museums = globaldata.data.filter(data => data.Type=='museum');
 
 			// map the data	
-			mapCSV(shops, shopmarkers, '#1681c4', 'Shops');
+			mapCSV(shops, shopmarkers, 'Shops', shopIcon);
+			mapCSV(parks, parkmarkers, 'Parks', parkIcon);
+			mapCSV(museums, museummarkers, 'Museums/Galleries', museumIcon);
 
 			let layers = {
-				"Shops": shopmarkers
+				"Shops": shopmarkers,
+				"Parks": parkmarkers,
+				"Museums/Galleries": museummarkers
 			};
 
-			//L.control.layers(null, layers).addTo(map);
+			L.control.layers(null, layers,{collapsed:false}).addTo(map);
 
 		}
 	})
 };
 
-function mapCSV(data, featuregroup, color, name){
+//icons
 
-	// icon
-	var shopIcon = L.icon({
-		iconUrl: 'icons/shops.png',
-		iconSize: [30,30]
-	})
+var shopIcon = L.icon({
+	iconUrl: 'icons/shops.png',
+	iconSize: [30,30]
+})
 
+var parkIcon = L.icon({
+	iconUrl: 'icons/parks.png',
+	iconSize: [30,30]
+})
+
+var museumIcon = L.icon({
+	iconUrl: 'icons/museums.png',
+	iconSize: [30,30]
+})
+
+
+function mapCSV(data, featuregroup, name, iconName){
 
 	// loop through each entry
 	data.forEach(function(item,index){
 		// create marker
-		let marker = L.marker([item.Latitude,item.Longitude],{icon: shopIcon})
+		let marker = L.marker([item.Latitude,item.Longitude],{icon: iconName})
 		.on('mouseover', function(){
 			this.bindPopup(`<h3>${item.Name}</h3><p>${item.Description}</p>`).openPopup()
 		})
